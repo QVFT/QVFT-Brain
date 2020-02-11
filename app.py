2c31f14306ef1c3d29c0f58da14522204c819197
Abu <abdulelah.rizan@gmail.com>
2020-02-10 21:38:38 -0500

    connects to postgres!

--- app.py
+++ app.py
@@ -1,29 +1,28 @@
-from flask import Flask
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
-from flask_script import Manager
-from flask_migrate import Migrate, MigrateCommand
+from flask_migrate import Migrate
 
 app = Flask(__name__)
-
 
 app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:farmingfam@localhost:5432/farmingdb'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 db = SQLAlchemy(app)
 
-import models
-
 migrate = Migrate(app, db)
-manager = Manager(app)
-manager.add_command('db', MigrateCommand)
+
+import models
 
 @app.route('/')
 def hello():
     return "Hello World!"
 
-@app.route('/<name>')
-def hello_name(name):
-    return "Hello {}!".format(name)
+@app.route("/readings/", methods=['POST']))
+def readings():
+    print (request.is_json)
+    content = request.get_json()
+    print (content)
+    return 'JSON posted'
 
 if __name__ == '__main__':
-    manager.run()
+    app.run(host="127.0.0.1", port='8080',debug=True)
--- models.py
+++ models.py
@@ -1,23 +1,38 @@
 from app import db
-
-class Target():
+from datetime import datetime
+class target(db.Model):
   #  Env_var_i
-    Target_Val = db.Column(db.Integer())
-    Time_from = db.Column(db.DateTime())
-    Time_to = db.Column(db.DateTime())
+    id = db.Column(db.Integer(), primary_key=True)
+    target_Val = db.Column(db.Integer())
+    time_from = db.Column(db.DateTime())
+    time_to = db.Column(db.DateTime())
 
-class User():
+class user(db.Model):
     user_id = db.Column(db.Integer(), primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(128))
 
-class Target_cycle():
-    ID = db.Column(db.Integer(), primary_key=True)
+class target_cycle(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
    # Target = 
     time_of_day = db.Column(db.DateTime())
    # account_id = 
 
-class Readings():
-    time = db.Column(db.DateTime())
+class env_var(db.Model):
+    label = db.Column(db.String(), primary_key=True)
+    unit = db.Column(db.String())
+
+class readings(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    time = db.Column(
+        db.DateTime, nullable=False, default=datetime.utcnow)
     #Env_var_id =
     var_val = db.Column(db.Integer())
+    env_var_id = db.Column(db.String(),db.ForeignKey("env_var.label"))
+    env_var = db.relationship(
+       'env_var', backref='Readings')
+
+
+    
+
+    
