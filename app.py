08dc933a66c7b563b3681c6c50e9c10d4f2eaa0e
Abu <abdulelah.rizan@gmail.com>
2020-03-04 20:05:37 -0500

    working db connection!

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask_migrate import Migrate
 app = Flask(__name__)
 
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:farmingfam@localhost:5432/farmingdb'
+app.config['SQLALCHEMY_DATABASE_URI']='postgres://vpqfujcg:dsQ1MyhV-Yux8uDp4WH7CKJQHKBaOLVv@drona.db.elephantsql.com:5432/vpqfujcg'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 db = SQLAlchemy(app)
 
@@ -17,12 +17,17 @@ import models
 def hello():
     return "Hello World!"
 
-@app.route("/readings/", methods=['POST']))
-def readings():
+@app.route("/api/readings/<label>", methods=['POST'])
+def readings(label):
     print (request.is_json)
     content = request.get_json()
     print (content)
     return 'JSON posted'
+    add_reading = models.readings(var_val=content['var_val'], env_var_id=content['env_var_id'], shelf_id=content['shelf_id'], sensor_id=content['sensor_id'], arduino_id=content['arduino_id'])
+    db.session.add(add_reading)
+    db.session.commit()
+    print(models.readings.query.all())
+
 
 if __name__ == '__main__':
-    app.run(host="127.0.0.1", port='8080',debug=True)
+    app.run(host="0.0.0.0", port='8080',debug=True)
